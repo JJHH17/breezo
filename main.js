6dd7d61e99beffca36757ac450428c8769889b44
James Hatfield <james.hatfield17@icloud.com>
2025-05-21 14:16:06 +0100

    Added type=module to html to support importing functions from alternative JS file

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-// File responsible for main app functionality, includes utilization of the Visual Crossing API.
+import { hideInitModal } from "./querySelectors.js";
 
+// File responsible for main app functionality, includes utilization of the Visual Crossing API.
+hideInitModal();
