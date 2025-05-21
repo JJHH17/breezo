c37421c53aceda544b48aafaa316b202bcc31d2c
James Hatfield <james.hatfield17@icloud.com>
2025-05-21 07:46:47 +0100

    Add query selector elements for initial modal elements

---
+++ querySelectors.js
@@ -0,0 +1,13 @@
+// File responsible for selecting front end elements
+
+// Selects the initial search input
+export function initInput() {
+    const initialInput = document.querySelector("#initInput");
+    return initInput;
+}
+
+// Selects initial search button
+export function initBtn() {
+    const initialButton = document.querySelector("#initBtn");
+    return initialButton;
+}
