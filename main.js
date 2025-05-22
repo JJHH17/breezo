bae1a660f7a1915961055514886844e4708a87d7
James Hatfield <james.hatfield17@icloud.com>
2025-05-22 10:01:43 +0100

    Import hide modal functions to main file

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import { initBtn, hideInitModal } from "./querySelectors.js";
+import { initBtn, hideInitModal, hideMainContent, hideHeaderSearch } from "./querySelectors.js";
+
+// Hides main content and header search on initial load
+hideMainContent();
 
 // File responsible for main app functionality, includes utilization of the Visual Crossing API.
 initBtn().addEventListener("click", () => {
