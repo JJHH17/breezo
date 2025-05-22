a5c88001385b8023d4a7b8a323918bbbef1bfe9a
James Hatfield <james.hatfield17@icloud.com>
2025-05-22 10:45:27 +0100

    Add API fetch function

--- main.js
+++ main.js
@@ -12,4 +12,20 @@ function getWeather(location) {
     // Display loading message here 
 
     // Fetching data
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=uk&include=days%2Chours&key=${apiKey}&contentType=json`, {
+        "method": "GET",
+        "mode": "cors",
+    })
+
+    // Parse to JSON
+    .then(function(response) {
+        return response.json();
+    })
+
+    // Return data to front end 
+    .then(function(response) {
+        console.log(response);
+    })
 }
+
+// Initial modal search
