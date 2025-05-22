9f8f5f9095d59830840bebd18dedf777310f6afc
James Hatfield <james.hatfield17@icloud.com>
2025-05-22 10:33:21 +0100

    Hide API key, fetch from env file

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 import { initBtn, hideInitModal, hideMainContent, hideHeaderSearch } from "./querySelectors.js";
 
+// Handling Visual Crossing API Credentials
+const apiKey = process.env.API_KEY;
+
 // Hides main content and header search on initial load
 hideMainContent();
 hideHeaderSearch();
 
-// File responsible for main app functionality, includes utilization of the Visual Crossing API.
-initBtn().addEventListener("click", () => {
-    
-})
+// API functionality 
+function getWeather(location) {
+    // Display loading message here 
+
+    // Fetching data
+}
